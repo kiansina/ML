23246334081b344299c5aa4643025f8ed563e970
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-04-29 16:39:00 +0200

    Add files via upload

---
+++ 16ML_Confusion_matrix.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_digits
+from sklearn.dummy import DummyClassifier
+from sklearn.svm import SVC
+
+dataset = load_digits()
+X, y = dataset.data, dataset.target
+for class_name, class_count in zip(dataset.target_names, np.bincount(dataset.target)):
+    print(class_name,class_count)
+
+# Creating a dataset with imbalanced binary classes:
+# Negative class (0) is 'not digit 1'
+# Positive class (1) is 'digit 1'
+y_binary_imbalanced = y.copy()
+y_binary_imbalanced[y_binary_imbalanced != 1] = 0
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_binary_imbalanced, random_state=0)
+
+from sklearn.metrics import confusion_matrix
+
+# Negative class (0) is most frequent
+dummy_majority = DummyClassifier(strategy = 'most_frequent').fit(X_train, y_train)
+y_majority_predicted = dummy_majority.predict(X_test)
+confusion = confusion_matrix(y_test, y_majority_predicted)
+
+print('Most frequent class (dummy classifier)\n', confusion)
+
+
+
+# produces random predictions w/ same class proportion as training set
+dummy_classprop = DummyClassifier(strategy='stratified').fit(X_train, y_train)
+y_classprop_predicted = dummy_classprop.predict(X_test)
+confusion = confusion_matrix(y_test, y_classprop_predicted)
+
+print('Random class-proportional prediction (dummy classifier)\n', confusion)
+
+
+
+svm = SVC(kernel='linear', C=1).fit(X_train, y_train)
+svm_predicted = svm.predict(X_test)
+confusion = confusion_matrix(y_test, svm_predicted)
+
+print('Support vector machine classifier (linear kernel, C=1)\n', confusion)
+
+
+
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression().fit(X_train, y_train)
+lr_predicted = lr.predict(X_test)
+confusion = confusion_matrix(y_test, lr_predicted)
+
+print('Logistic regression classifier (default settings)\n', confusion)
+
+
+
+
+from sklearn.tree import DecisionTreeClassifier
+
+dt = DecisionTreeClassifier(max_depth=2).fit(X_train, y_train)
+tree_predicted = dt.predict(X_test)
+confusion = confusion_matrix(y_test, tree_predicted)
+
+print('Decision tree classifier (max_depth = 2)\n', confusion)
